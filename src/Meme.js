6ddec939cd6a76e6087c6be1e6dfa55e0b25236b
Quentin Ikeno <ikenoq@gmail.com>
2022-08-02 19:35:09 -1000

    Add button to remove Meme from DOM and state.

--- src/Meme.js
+++ src/Meme.js
@@ -1,13 +1,22 @@
+import { useDispatch } from "react-redux";
+import { remove } from "./features/memesSlice";
 import "./Meme.css";
 
 const Meme = ({ meme }) => {
-	const { imgURL, topText, bottomText } = meme;
+	const { id, imgURL, topText, bottomText } = meme;
+	const dispatch = useDispatch();
+
+	const handleDelete = () => {
+		dispatch(remove(id));
+	};
+
 	return (
-		<figure className="image column is-full-mobile is-half-tablet">
+		<div className="image column is-full-mobile is-half-tablet">
+			<button className="delete is-large" onClick={handleDelete}></button>
 			{topText && <p className="topText">{topText}</p>}
 			<img src={imgURL} alt="A fresh new meme." />
 			{bottomText && <p className="bottomText">{bottomText}</p>}
-		</figure>
+		</div>
 	);
 };
 
